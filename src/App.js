c8c3c37f36e7d417071cb3ee139a68b44e2552db
Eddysam2000 <samueledwin67@gmail.com>
2023-12-07 12:05:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from "react-chat-engine";
+import  ChatFeed  from "./components/ChatFeed";
+
 import './App.css';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+        height="100vh" 
+        projectID="bd927b89-63da-4e11-85e6-c6b9291223a6"
+        userName="Eddysam"
+        userSecret="1234"
+        renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps } />} 
+    
+    />
   );
 }
 
-export default App;
+export default App
