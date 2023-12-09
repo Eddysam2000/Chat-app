b7de00d15406296e72315795fb3d2255f2296da7
Eddysam2000 <samueledwin67@gmail.com>
2023-12-10 00:21:55 +0100

    loginform created

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,7 +6,11 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = () => {}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': "bd927b89-63da-4e11-85e6-c6b9291223a6" }
+    }
 
     return (
         <div className='wrapper'>
