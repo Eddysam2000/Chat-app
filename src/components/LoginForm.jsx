38c410afcade5178823978162ff3c55a46fbd74c
Eddysam2000 <samueledwin67@gmail.com>
2023-12-10 00:13:44 +0100

    loginform created

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,13 +6,16 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (data) => {}
+    const handleSubmit = () => {}
 
     return (
         <div className='wrapper'>
             <div className='form'>
                 <h1 className='title'>Chat Application</h1>
-                <form onSubmit={handleSubmit}></form>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} className='input' placeholder='Username' required />
+                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='input' placeholder='Password' required />
+                </form>
             </div>
         </div>
     )
