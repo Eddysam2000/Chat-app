6f2c8eb91eff4c6fa92379299d73027709d0bf11
Eddysam2000 <samueledwin67@gmail.com>
2023-12-09 14:12:14 +0100

    loginform created

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { useState } from 'react'
+import axios from 'axios';
 
 const LoginForm = () => {
     const [username, setUsername] = useState('');
-    const [password, setpassword] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (data) => {}
+
+    return (
+        <div className='wrapper'>
+            <div className='form'>
+                <h1 className='title'>Chat Application</h1>
+                <form onSubmit={handleSubmit}></form>
+            </div>
+        </div>
+    )
 }
 
 export default LoginForm
