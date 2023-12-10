a51078c6c8d22fd5aea7b20beded30ca25ec3628
Eddysam2000 <samueledwin67@gmail.com>
2023-12-11 00:05:10 +0100

    loginform created

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,10 +6,16 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const authObject = { 'Project-ID': "bd927b89-63da-4e11-85e6-c6b9291223a6" }
+        const authObject = { 'Project-ID': "bd927b89-63da-4e11-85e6-c6b9291223a6", 'User-Name': username, 'User-Secret': password }
+
+        try {
+            axios.get('')
+        } catch (error) {
+            
+        }
     }
 
     return (
