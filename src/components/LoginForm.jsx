552f36f0d5f5e95d639c4e1bf40cdc11027814b1
Eddysam2000 <samueledwin67@gmail.com>
2023-12-11 10:00:09 +0100

    loginform created

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -37,6 +37,7 @@ const LoginForm = () => {
                             <span>Start Chatting</span>
                         </button>
                     </div>
+                    <h2 className='error'>{error}</h2>
                 </form>
             </div>
         </div>
